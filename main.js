64e964ea0b20229c0d196cff404eab301a39676f
Xuri <xuriwork@gmail.com>
2020-06-08 18:33:03 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+const toggleMenu = () => {
+    const mobilemenu = document.getElementById('mobile-menu');
+    if (mobilemenu.style.display === 'block') mobilemenu.style.display = 'none';
+    else mobilemenu.style.display = 'block';
+};
